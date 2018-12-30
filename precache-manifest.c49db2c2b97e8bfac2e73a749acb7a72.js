7c2e7751eb17b68c827d3d1bfb9c80b0bec3491c
Harald Rudell <harald.rudell@gmail.com>
2018-12-29 17:05:51 -0800

    Update

--- precache-manifest.c49db2c2b97e8bfac2e73a749acb7a72.js
+++ precache-manifest.c49db2c2b97e8bfac2e73a749acb7a72.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "931aad4180375e0f9962",
-    "url": "/haraldrudell/static/js/main.931aad41.chunk.js"
+    "revision": "8c614df82518db9cc07c",
+    "url": "/haraldrudell/static/js/main.8c614df8.chunk.js"
   },
   {
     "revision": "b87e8976e4d0e3915095",
@@ -12,7 +12,7 @@ self.__precacheManifest = [
     "url": "/haraldrudell/static/js/runtime~main.4fd5a8ba.js"
   },
   {
-    "revision": "6b5e58d3fe0b21b7cbe1807351b492fd",
+    "revision": "bca147deb0e76b99a06f9d5f505feb33",
     "url": "/haraldrudell/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/haraldrudell/precache-manifest.82b7b7dca10e6943b57e084cce034a83.js"
+  "/haraldrudell/precache-manifest.c49db2c2b97e8bfac2e73a749acb7a72.js"
 );
 
 workbox.clientsClaim();
